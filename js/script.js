37981aaf48706aaf984d482eafeb481269efd0a2
savvakisweb <manos.savvakis@outlook.com>
2024-01-04 21:08:31 +0200

    complete header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function runTypingEffect() {
+    const text = 'I am Manos Savvakis.';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 150;
+  
+    typeText(text, typingElement, typingDelay);
+  }
+  
+  function typeText(text, typingElement, delay) {
+    for (let i = 0; i < text.length; i++) {
+      setTimeout(() => {
+        typingElement.textContent += text.charAt(i);
+      }, delay * i);
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', runTypingEffect);
